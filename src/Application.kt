c24bc1e9cc9973a1c8738dc27d89f3a7986e201d
miyado <hmiyado@gmail.com>
2019-02-09 21:40:56 +0900

    apply application configuration

--- src/Application.kt
+++ src/Application.kt
@@ -1,24 +1,20 @@
 package com.github.hmiyado
 
-import io.ktor.application.*
-import io.ktor.response.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 
-fun main() {
-    embeddedServer(Netty) {
-        routing {
-            get("/") {
-                call.respondText("Hello World!")
-            }
-        }
-    }.start(wait = true)
-}
+fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
 }
 
