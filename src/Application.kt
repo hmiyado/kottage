1675273be16ac7214eef884f0dc0189752b8f84f
miyado <hmiyado@gmail.com>
2019-02-09 20:33:20 +0900

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,24 @@
+package com.github.hmiyado
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!")
+            }
+        }
+    }.start(wait = true)
+}
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+}
+
