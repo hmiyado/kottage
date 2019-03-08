2223f0141c1c910c93a60187447fbd53c86b2311
miyado <hmiyado@gmail.com>
2019-03-08 22:47:01 +0900

    add article repository mock implementation

--- src/infra/repositoryimplementation/ArticleRepositoryImpl.kt
+++ src/infra/repositoryimplementation/ArticleRepositoryImpl.kt
@@ -2,9 +2,12 @@ package com.github.hmiyado.infra.repositoryimplementation
 
 import com.github.hmiyado.model.Article
 import com.github.hmiyado.repository.ArticleRepository
+import java.time.ZonedDateTime
 
 class ArticleRepositoryImpl : ArticleRepository {
     override fun getArticles(): List<Article> {
-        return emptyList()
+        // return mock values until real database access implementation
+        return (0..10)
+            .map { Article("title $it", "body $it", ZonedDateTime.now()) }
     }
 }
