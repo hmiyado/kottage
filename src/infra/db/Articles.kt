1c64b6f846b6842e76405e58bbaf8448e434d572
miyado <hmiyado@gmail.com>
2021-04-29 17:51:11 +0900

    do not use deprecated method

--- src/infra/db/Articles.kt
+++ src/infra/db/Articles.kt
@@ -1,10 +1,9 @@
 package com.github.hmiyado.infra.db
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.`java-time`.datetime
 
-object Articles: Table() {
-    val id = integer("id").autoIncrement().primaryKey()
+object Articles : IntIdTable() {
     val title = varchar("title", 100)
     val body = text("body")
     val dateTime = datetime("dateTime")
