e663ee4f5ba4ca6d564f47d0d536e4787187bbc7
miyado <hmiyado@gmail.com>
2021-04-30 12:09:48 +0900

    add ArticlesRoutingTest

--- src/route/ArticlesRouting.kt
+++ src/route/ArticlesRouting.kt
@@ -5,11 +5,13 @@ import io.ktor.application.call
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.koin.ktor.ext.inject
 
 fun Route.articles() {
     val articleRepository: ArticleRepository by inject()
     get("articles") {
-        call.respond(articleRepository.getArticles())
+        call.respond(Json.encodeToString(articleRepository.getArticles()))
     }
 }
