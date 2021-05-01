22b2ba4253693e0e10214b9a7833a45ab94a537c
miyado <hmiyado@gmail.com>
2021-05-01 17:13:27 +0900

    add POST /articles

--- test/com/github/hmiyado/route/ArticlesRoutingKtTest.kt
+++ test/com/github/hmiyado/route/ArticlesRoutingKtTest.kt
@@ -2,30 +2,87 @@ package com.github.hmiyado.route
 
 import com.github.hmiyado.repository.repositoryModule
 import com.github.hmiyado.service.serviceModule
+import io.kotlintest.Spec
+import io.kotlintest.matchers.maps.shouldContainKey
+import io.kotlintest.matchers.maps.shouldNotContainAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.routing.routing
+import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.handleRequest
-import io.ktor.server.testing.withTestApplication
+import io.ktor.server.testing.setBody
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
 import org.koin.core.context.startKoin
 
-class ArticlesRoutingKtTest : DescribeSpec({
-    describe("com.github.hmiyado.route /articles") {
-        it("should return articles") {
-            startKoin {
-                modules(repositoryModule, serviceModule)
+class ArticlesRoutingKtTest : DescribeSpec() {
+    private lateinit var testApplicationEngine: TestApplicationEngine
+
+    override fun beforeSpec(spec: Spec) {
+        super.beforeSpec(spec)
+        startKoin {
+            modules(repositoryModule, serviceModule)
+        }
+        testApplicationEngine = TestApplicationEngine()
+        testApplicationEngine.start()
+        with(testApplicationEngine) {
+            application.routing {
+                articles()
             }
-            withTestApplication({
-                routing {
-                    articles()
+        }
+    }
+
+    override fun afterSpec(spec: Spec) {
+        super.afterSpec(spec)
+        testApplicationEngine.stop(0L, 0L)
+    }
+
+    init {
+        describe("route /articles") {
+            it("should return articles") {
+                with(testApplicationEngine) {
+                    with(handleRequest(HttpMethod.Get, "/articles")) {
+                        response.status() shouldBe HttpStatusCode.OK
+                    }
                 }
-            }) {
-                with(handleRequest(HttpMethod.Get, "/articles")) {
-                    response.status() shouldBe HttpStatusCode.OK
+            }
+        }
+
+        describe("route POST /articles") {
+            it("should return new article") {
+                with(testApplicationEngine) {
+                    with(handleRequest(HttpMethod.Post, "/articles") {
+                        val map = mapOf(
+                            "title" to "title1",
+                            "body" to "body1"
+                        )
+                        val body = Json.encodeToString(map)
+                        setBody(body)
+                    }) {
+                        response.status() shouldBe HttpStatusCode.OK
+                        val jsonBody =
+                            response.content?.let { Json.parseToJsonElement(it).jsonObject }?.toMap() ?: emptyMap()
+                        jsonBody shouldNotContainAll mapOf(
+                            "title" to "title1",
+                            "body" to "body1"
+                        )
+                        jsonBody shouldContainKey "dateTime"
+                    }
+                }
+            }
+
+            it("should return Bad Request") {
+                with(testApplicationEngine) {
+                    with(handleRequest(HttpMethod.Post, "/articles") {
+                        setBody("")
+                    }) {
+                        response.status() shouldBe HttpStatusCode.BadRequest
+                    }
                 }
             }
         }
     }
-})
+}
