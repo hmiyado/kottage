cfdee8fa453bc8cf224d975776841b30120c97b1
miyado <hmiyado@gmail.com>
2021-05-03 15:31:52 +0900

    update test case

--- test/com/github/hmiyado/application/DatabaseConfigurationTest.kt
+++ test/com/github/hmiyado/application/DatabaseConfigurationTest.kt
@@ -17,12 +17,16 @@ class DatabaseConfigurationTest : DescribeSpec() {
                 actual shouldBe DatabaseConfiguration.Postgres("name", "host", "user", "password")
             }
 
-            describe("should return Memory when there are lack of postgres properties") {
+            describe("should return Memory when there are null or empty postgres properties") {
                 forAll(
                     DatabaseConfigurationParameter(null, "host", "user", "password"),
                     DatabaseConfigurationParameter("name", null, "user", "password"),
                     DatabaseConfigurationParameter("name", "host", null, "password"),
                     DatabaseConfigurationParameter("name", "host", "user", null),
+                    DatabaseConfigurationParameter("", "host", "user", "password"),
+                    DatabaseConfigurationParameter("name", "", "user", "password"),
+                    DatabaseConfigurationParameter("name", "host", "", "password"),
+                    DatabaseConfigurationParameter("name", "host", "user", ""),
                 ) { (name, host, user, password) ->
                     val actual = DatabaseConfiguration.detectConfiguration(
                         name,
