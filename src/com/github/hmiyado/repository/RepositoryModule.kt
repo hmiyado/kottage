e6de07e8ff010e418558837c32430f4d46bea311
miyado <hmiyado@gmail.com>
2021-05-03 15:23:57 +0900

    use DatabaseConfiguration safely

--- src/com/github/hmiyado/repository/RepositoryModule.kt
+++ src/com/github/hmiyado/repository/RepositoryModule.kt
@@ -1,21 +1,17 @@
 package com.github.hmiyado.repository
 
 import com.github.hmiyado.application.DatabaseConfiguration
-import com.github.hmiyado.repository.articles.ArticleRepository
 import com.github.hmiyado.repository.articles.ArticleRepositoryDatabase
 import com.github.hmiyado.repository.articles.ArticleRepositoryOnMemory
 import com.github.hmiyado.repository.articles.Articles
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
-fun provideRepositoryModule(databaseConfiguration: DatabaseConfiguration) = module {
-    initializeDatabase(databaseConfiguration)
-    when (databaseConfiguration) {
-        DatabaseConfiguration.Memory -> {
-            single { ArticleRepositoryOnMemory() } bind ArticleRepository::class
-        }
-        is DatabaseConfiguration.Postgres -> {
-            single { ArticleRepositoryDatabase(Articles) } bind ArticleRepository::class
+val repositoryModule = module {
+    single { initializeDatabase(get()) }
+    single {
+        when (get<DatabaseConfiguration>()) {
+            DatabaseConfiguration.Memory -> ArticleRepositoryOnMemory()
+            is DatabaseConfiguration.Postgres -> ArticleRepositoryDatabase(Articles)
         }
     }
 }
