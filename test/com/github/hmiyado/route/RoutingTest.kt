b3a96a5620723bba2a51ba57b5af704daf71652e
miyado <hmiyado@gmail.com>
2021-05-04 18:36:10 +0900

    extract Allow header assertion

--- test/com/github/hmiyado/route/RoutingTest.kt
+++ test/com/github/hmiyado/route/RoutingTest.kt
@@ -2,10 +2,11 @@ package com.github.hmiyado.route
 
 import com.github.hmiyado.helper.AuthorizationHelper
 import com.github.hmiyado.service.articles.ArticlesService
-import io.kotest.assertions.ktor.shouldHaveHeader
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainExactly
 import io.ktor.http.HttpMethod
+import io.ktor.response.ApplicationResponse
 import io.ktor.routing.routing
 import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.handleRequest
@@ -52,14 +53,20 @@ class RoutingTest : DescribeSpec(), KoinTest {
 
     init {
         describe("/") {
-            it("should allow GET") {
+            it("should allow OPTIONS GET") {
                 testApplicationEngine.application
                 testApplicationEngine
                     .handleRequest(HttpMethod.Options, "/")
                     .run {
-                        response.shouldHaveHeader("Allow", "OPTIONS, GET")
+                        response.shouldAllowMethods(HttpMethod.Options, HttpMethod.Get)
                     }
             }
         }
+    }
+
+    private fun ApplicationResponse.shouldAllowMethods(vararg methods: HttpMethod) {
+        val allowedMethods =
+            headers["Allow"]?.split(",")?.map { it.trim() }?.map { HttpMethod.parse(it) } ?: emptyList()
+        allowedMethods.shouldContainExactly(*methods)
     }
 }
