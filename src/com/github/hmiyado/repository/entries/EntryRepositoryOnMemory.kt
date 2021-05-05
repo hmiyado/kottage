2385d036b569eda3e9252321447018c169886972
miyado <hmiyado@gmail.com>
2021-05-05 11:38:44 +0900

    rename Article to Entry in EntryRepositoryOnMemory.kt

--- src/com/github/hmiyado/repository/entries/EntryRepositoryOnMemory.kt
+++ src/com/github/hmiyado/repository/entries/EntryRepositoryOnMemory.kt
@@ -4,37 +4,37 @@ import com.github.hmiyado.model.Entry
 import java.time.ZonedDateTime
 
 class EntryRepositoryOnMemory : EntryRepository {
-    private val articles = (0..10)
+    private val entries = (0..10)
         .map { Entry(it.toLong(), "title $it", "body $it", ZonedDateTime.now()) }
         .toMutableList()
 
     override fun getEntries(): List<Entry> {
-        return articles
+        return entries
     }
 
     override fun createEntry(title: String, body: String): Entry {
-        val article = Entry((articles.size + 1).toLong(), title, body, ZonedDateTime.now())
-        articles += article
+        val article = Entry((entries.size + 1).toLong(), title, body, ZonedDateTime.now())
+        entries += article
         return article
     }
 
     override fun getEntry(serialNumber: Long): Entry? {
-        return articles.find { it.serialNumber == serialNumber }
+        return entries.find { it.serialNumber == serialNumber }
     }
 
     override fun updateEntry(serialNumber: Long, title: String?, body: String?): Entry? {
-        val tmp = articles.map {
+        val tmp = entries.map {
             if (it.serialNumber != serialNumber) {
                 return@map it
             }
             return@map it.copy(title = title ?: it.title, body = body ?: it.body)
         }
-        articles.removeAll { true }
-        articles.addAll(tmp)
-        return articles.find { it.serialNumber == serialNumber }
+        entries.removeAll { true }
+        entries.addAll(tmp)
+        return entries.find { it.serialNumber == serialNumber }
     }
 
     override fun deleteEntry(serialNumber: Long) {
-        articles.removeIf { it.serialNumber == serialNumber }
+        entries.removeIf { it.serialNumber == serialNumber }
     }
 }
