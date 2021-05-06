9bfe8aa280a2bbbcd01a1d89bb7fc5701396f00b
miyado <hmiyado@gmail.com>
2021-05-06 20:54:41 +0900

    arrange better process

--- scripts/run-e2e-test-on-docker.sh
+++ scripts/run-e2e-test-on-docker.sh
@@ -1,11 +1,2 @@
-# sh scripts/run-on-docker.sh
-# sh scripts/wait-starting-server.sh
-#if [ $? -ne 0 ]; then
-#  # server is not running
-#  docker-compose stop
-#  exit 1
-#fi
-
 docker build -t karate -f karate.Dockerfile .
 docker run --rm --network container:kottage karate gradle karate:test
-docker-compose stop
