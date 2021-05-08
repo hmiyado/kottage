414f2ec38a4561bb946ba2cd09ddc4e86f9a63ac
miyado <hmiyado@gmail.com>
2021-05-08 12:06:47 +0900

    use karate.Dockerfile.dockerignore instead of .dockerignore

--- scripts/run-e2e-test-on-docker.sh
+++ scripts/run-e2e-test-on-docker.sh
@@ -1,2 +1,4 @@
-docker build -t karate -f karate.Dockerfile .
+# DOCKER_BUILDKIT=1 is need to use karate.Dockerfile.dockerignore.
+# https://docs.docker.com/engine/reference/commandline/build/#use-a-dockerignore-file
+DOCKER_BUILDKIT=1 docker build -t karate -f karate.Dockerfile .
 docker run --rm --network container:kottage karate gradle karate:test
