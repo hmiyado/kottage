3b9efc74b981c273545633ce171290668461182d
miyado <hmiyado@gmail.com>
2021-06-20 20:09:28 +0900

    add contact page

---
+++ pages/contact/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Contact() {
+  return (
+    <div>
+      <Head>
+        <title>Contact</title>
+      </Head>
+
+      <main>
+        <h1>Contact</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
