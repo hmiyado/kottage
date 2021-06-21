4ac1d2971d332ad47258a221ff7aa343eedbfaee
miyado <hmiyado@gmail.com>
2021-06-21 19:54:38 +0900

    redirect / to /contact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/contact',
+        permanent: true,
+      },
+    ]
+  },
 }
