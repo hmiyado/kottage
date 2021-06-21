8d2849c0c0d445e0658701797b70242d89d2be58
miyado <hmiyado@gmail.com>
2021-06-21 13:49:23 +0900

    create Contact.module.css

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,12 +1,14 @@
+import styles from '../styles/Contact.module.css'
+
 export default function Contact() {
   return (
-    <section className="bg-white px-ll py-m">
-      <h2 className="text-headline5">Skill</h2>
-      <p>resume</p>
-      <h2>Price</h2>
-      <p>詳細要相談</p>
-      <h2>Contact</h2>
-      <p></p>
+    <section className={styles.section}>
+      <h2 className={styles.headline}>Skill</h2>
+      <p className={styles.body}>resume</p>
+      <h2 className={styles.headline}>Price</h2>
+      <p className={styles.body}>詳細要相談</p>
+      <h2 className={styles.headline}>Contact</h2>
+      <p className={styles.body}></p>
     </section>
   )
 }
