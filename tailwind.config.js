162f6823788fb94d4159ad98f68907df09c4c419
miyado <hmiyado@gmail.com>
2021-06-21 20:05:54 +0900

    Layout become responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { screens: {} },
     colors: {
       white: '#FFFFFF',
       primary: {
