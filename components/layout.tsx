44eeda5bce2e7d4612a68a9c516c3d2073af214e
miyado <hmiyado@gmail.com>
2021-06-27 22:28:04 +0900

    remove unnecessary div

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Layout.module.css'
 
 export default function Layout({ children }: { children: JSX.Element }) {
   return (
-    <div className={styles.root}>
+    <>
       <Head>
         <title>Book of Days</title>
       </Head>
@@ -12,6 +12,6 @@ export default function Layout({ children }: { children: JSX.Element }) {
       </header>
 
       <main className={styles.main}>{children}</main>
-    </div>
+    </>
   )
 }
