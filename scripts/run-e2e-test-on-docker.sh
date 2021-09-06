83e26d1f69d5f55536806281bdb4543bf0a077cc
miyado <hmiyado@gmail.com>
2021-09-06 18:10:36 +0900

    add shebang

--- scripts/run-e2e-test-on-docker.sh
+++ scripts/run-e2e-test-on-docker.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 # DOCKER_BUILDKIT=1 is need to use karate.Dockerfile.dockerignore.
 # https://docs.docker.com/engine/reference/commandline/build/#use-a-dockerignore-file
 DOCKER_BUILDKIT=1 docker build -t karate -f karate.Dockerfile .
