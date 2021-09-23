e2891a67c289baa1103da9bf3bbd7c5ced03fc4f
miyado <hmiyado@gmail.com>
2021-09-23 20:34:03 +0900

    fix deprecated import

--- src/main/kotlin/com/github/hmiyado/kottage/repository/entries/Entries.kt
+++ src/main/kotlin/com/github/hmiyado/kottage/repository/entries/Entries.kt
@@ -3,7 +3,7 @@ package com.github.hmiyado.kottage.repository.entries
 import com.github.hmiyado.kottage.repository.users.Users
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
-import org.jetbrains.exposed.sql.`java-time`.datetime
+import org.jetbrains.exposed.sql.javatime.datetime
 
 object Entries : LongIdTable() {
     val title = varchar("title", 100)
