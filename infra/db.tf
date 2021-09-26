8f4db6d8bc2ff2a85d5c9143ad8328a2d4e0472f
miyado <hmiyado@gmail.com>
2021-09-26 18:20:18 +0900

    create db

---
+++ infra/db.tf
@@ -0,0 +1,23 @@
+resource "aws_db_instance" "kottage_db" {
+  allocated_storage          = 20
+  engine                     = "mysql"
+  instance_class             = "db.t2.micro"
+  name                       = "kottage"
+  username                   = "user"
+  password                   = "password"
+  parameter_group_name       = "default.mysql8.0"
+  skip_final_snapshot        = true
+  availability_zone          = var.availability_zones[0]
+  auto_minor_version_upgrade = true
+  vpc_security_group_ids     = [aws_security_group.kottage_db.id]
+  db_subnet_group_name       = aws_db_subnet_group.kottage.name
+}
+
+resource "aws_db_subnet_group" "kottage" {
+  name       = "kottage"
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = {
+    Name = "kottage"
+  }
+}
