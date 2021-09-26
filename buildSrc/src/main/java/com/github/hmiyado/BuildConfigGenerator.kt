d9d2c74317b9efc408e0c0f72890801984c64566
miyado <hmiyado@gmail.com>
2021-09-26 15:39:38 +0900

    /health returns git hash

--- buildSrc/src/main/java/com/github/hmiyado/BuildConfigGenerator.kt
+++ buildSrc/src/main/java/com/github/hmiyado/BuildConfigGenerator.kt
@@ -4,7 +4,9 @@ import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
 
 object BuildConfigGenerator {
     fun generate(destination: File, version: String? = null) {
@@ -14,7 +16,7 @@ object BuildConfigGenerator {
                 TypeSpec.objectBuilder(className)
                     .addProperty(
                         PropertySpec.builder("version", String::class, KModifier.CONST)
-                            .initializer((version ?: "0.0.0").wrappedRawLiteral())
+                            .initializer("${version ?: "0.0.0"}+${getGitHash()}".wrappedRawLiteral())
                             .build()
                     )
                     .build()
@@ -25,5 +27,19 @@ object BuildConfigGenerator {
 
     private fun String.wrappedRawLiteral(): String {
         return "\"${this}\""
+    }
+
+    private fun getGitHash(): String {
+        val process = ProcessBuilder("git", "rev-parse", "--short", "HEAD")
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .start()
+        process.waitFor()
+
+        return try {
+            val br = BufferedReader(InputStreamReader(process.inputStream))
+            br.readLine()
+        } catch (e: Throwable) {
+            "nogithash"
+        }
     }
 }
