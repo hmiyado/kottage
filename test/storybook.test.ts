6b704c0a2c2ddfaae212f09ad17e01edb9a49803
miyado <hmiyado@gmail.com>
2021-10-04 20:32:33 +0900

    configure jest

---
+++ test/storybook.test.ts
@@ -0,0 +1,9 @@
+// import initStoryshots from '@storybook/addon-storyshots'
+// initStoryshots()
+import '@jest/globals'
+
+describe('jest 動作確認', () => {
+  test('true toBeTruthy', () => {
+    expect(true).toBeTruthy()
+  })
+})
