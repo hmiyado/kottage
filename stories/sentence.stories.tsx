e638a76ede3ef857fd588e72e652243f26520251
miyado <hmiyado@gmail.com>
2021-10-04 19:57:37 +0900

    replace stories with real components

---
+++ stories/sentence.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Sentence from '../components/sentence'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+export default {
+  title: 'Sentence',
+  component: Sentence,
+  argTypes: {},
+} as ComponentMeta<typeof Sentence>
+
+const Template: ComponentStory<typeof Sentence> = (args) => (
+  <Sentence {...args} />
+)
+
+export const Short = Template.bind({})
+Short.args = {
+  title: 'short title',
+}
+
+export const Long = Template.bind({})
+Long.args = {
+  title: 'long long long long long long title',
+}
