c1547701e7119ee3461f6fe5a5c18e2d145679ff
miyado <hmiyado@gmail.com>
2021-10-04 21:24:14 +0900

    update storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,22 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  typescript: { reactDocgen: false },
 }
