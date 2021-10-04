0025ef7661ac18f668142fc142506debb2bd59df
miyado <hmiyado@gmail.com>
2021-10-04 16:08:51 +0900

    remove unnecessary comments

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -48,24 +48,13 @@ resource "aws_ecs_service" "kottage_api" {
   task_definition = aws_ecs_task_definition.kottage_api.arn
   launch_type     = "FARGATE"
   desired_count   = 1
-  //  iam_role        = aws_iam_role.foo.arn
   depends_on = [aws_security_group_rule.kottage]
 
-  //  ordered_placement_strategy {
-  //    type  = "binpack"
-  //    field = "cpu"
-  //  }
-  //
   load_balancer {
     target_group_arn = aws_lb_target_group.lb_target_kottage_api.arn
     container_name   = "kottage_api"
     container_port   = 8080
   }
-
-  //  placement_constraints {
-  //    type       = "memberOf"
-  //    expression = "attribute:ecs.availability-zone in [us-east-2a, us-east-2b]"
-  //  }
 
   network_configuration {
     subnets         = aws_subnet.private.*.id
