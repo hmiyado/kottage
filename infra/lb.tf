c8586989a780d6deaa176846e2d8b053700f1a98
miyado <hmiyado@gmail.com>
2021-10-04 16:20:47 +0900

    lb does not accept HTTP

--- infra/lb.tf
+++ infra/lb.tf
@@ -28,17 +28,6 @@ resource "aws_lb_target_group" "lb_target_kottage_api" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.lb_target_kottage_api.arn
-  }
-}
-
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "443"
