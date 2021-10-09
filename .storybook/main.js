e129c29bcc0d1c220b4962111feab1a81e1c72d2
miyado <hmiyado@gmail.com>
2021-10-09 21:14:21 +0900

    enable storybook with tailwindcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   core: {
     builder: 'webpack5',
@@ -6,17 +8,23 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
         },
-      },
-    },
-  ],
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  },
   typescript: { reactDocgen: false },
 }
