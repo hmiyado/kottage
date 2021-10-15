4f32cd2f74a941be5ebf4859676ca38c48f202cc
miyado <hmiyado@gmail.com>
2021-10-15 09:22:26 +0900

    remove unnecessary sotries reference

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   stories: [
     '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
