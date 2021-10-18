b7a4ce3ddd9488d8817f55bd5b303534854e2eea
miyado <hmiyado@gmail.com>
2021-10-18 17:28:55 +0900

    add root page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout/layout'
+import Contact from '../components/contact/contact'
+
+export default function RootPage() {
+  return (
+    <Layout>
+      {/* TODO: Contact is a temporally content */}
+      <Contact />
+    </Layout>
+  )
+}
