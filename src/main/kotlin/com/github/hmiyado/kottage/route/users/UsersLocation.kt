6767f86607e25980d4cbf3cf48931a40ff9885d0
miyado <hmiyado@gmail.com>
2021-10-31 20:40:59 +0900

    use openapi endpoints

--- src/main/kotlin/com/github/hmiyado/kottage/route/users/UsersLocation.kt
+++ src/main/kotlin/com/github/hmiyado/kottage/route/users/UsersLocation.kt
@@ -2,17 +2,15 @@ package com.github.hmiyado.kottage.route.users
 
 import com.github.hmiyado.kottage.model.User
 import com.github.hmiyado.kottage.model.UserSession
-import com.github.hmiyado.kottage.openapi.Paths
-import com.github.hmiyado.kottage.openapi.models.SignInPostRequest
-import com.github.hmiyado.kottage.openapi.models.UsersPostRequest
+import com.github.hmiyado.kottage.openapi.apis.OpenApi
 import com.github.hmiyado.kottage.route.Path
 import com.github.hmiyado.kottage.route.allowMethods
-import com.github.hmiyado.kottage.route.receiveOrThrow
 import com.github.hmiyado.kottage.service.users.UsersService
 import io.ktor.application.call
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.pathComponents
+import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.response.header
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -25,7 +23,9 @@ import io.ktor.sessions.set
 import io.ktor.util.url
 import com.github.hmiyado.kottage.openapi.models.User as ResponseUser
 
+
 class UsersLocation {
+    @KtorExperimentalLocationsAPI
     companion object {
         private fun User.toResponseUser() = ResponseUser(screenName = screenName, id = id)
 
@@ -34,30 +34,30 @@ class UsersLocation {
                 val users = usersService.getUsers()
                 call.respond(users)
             }
-            post(Paths.usersPost) {
-                val (screenName, password) = call.receiveOrThrow<UsersPostRequest>()
-                val user = try {
-                    usersService.createUser(screenName, password)
-                } catch (e: UsersService.DuplicateScreenNameException) {
-                    call.respond(HttpStatusCode.BadRequest)
-                    return@post
-                }
-                call.response.header("Location", this.context.url { this.pathComponents("/${user.id}") })
-                call.sessions.set(UserSession(id = user.id))
-                call.respond(HttpStatusCode.Created, user.toResponseUser())
-            }
 
-            post(Paths.signInPost) {
-                val (screenName, password) = call.receiveOrThrow<SignInPostRequest>()
-                val user = usersService.authenticateUser(screenName, password)
-                if (user == null) {
-                    call.respond(HttpStatusCode.NotFound)
-                    return@post
+            with(OpenApi) {
+                usersPost { (screenName, password) ->
+                    val user = try {
+                        usersService.createUser(screenName, password)
+                    } catch (e: UsersService.DuplicateScreenNameException) {
+                        call.respond(HttpStatusCode.BadRequest)
+                        return@usersPost
+                    }
+                    call.response.header("Location", this.context.url { this.pathComponents("/${user.id}") })
+                    call.sessions.set(UserSession(id = user.id))
+                    call.respond(HttpStatusCode.Created, user.toResponseUser())
                 }
-                call.sessions.set(UserSession(id = user.id))
-                call.respond(HttpStatusCode.OK, user.toResponseUser())
-            }
 
+                signInPost { (screenName, password) ->
+                    val user = usersService.authenticateUser(screenName, password)
+                    if (user == null) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@signInPost
+                    }
+                    call.sessions.set(UserSession(id = user.id))
+                    call.respond(HttpStatusCode.OK, user.toResponseUser())
+                }
+            }
             post(Path.SignOut) {
                 call.sessions.clear<UserSession>()
                 call.respond(HttpStatusCode.OK)
