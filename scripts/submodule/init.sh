51a6c61bb9e1f1f5bc35934a692ae87c8cd40dc2
miyado <hmiyado@gmail.com>
2021-11-03 11:17:30 +0900

    enable sparsecheckout in submodule when initialize

--- scripts/submodule/init.sh
+++ scripts/submodule/init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 git submodule update --init --recursive
+git -C kottage config core.sparsecheckout true
 cp scripts/submodule/sparse-checkout .git/modules/kottage/info/sparse-checkout
 git -C kottage read-tree -mu HEAD
