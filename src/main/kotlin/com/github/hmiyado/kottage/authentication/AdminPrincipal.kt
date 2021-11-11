23e29d6327aea90b0e50e8c1599f67221a25af82
miyado <hmiyado@gmail.com>
2021-11-11 22:30:08 +0900

    remove unused file, method, properties

--- src/main/kotlin/com/github/hmiyado/kottage/authentication/AdminPrincipal.kt
+++
@@ -1,6 +0,0 @@
-package com.github.hmiyado.kottage.authentication
-
-import io.ktor.auth.Principal
-
-
-object AdminPrincipal : Principal
--- src/test/kotlin/com/github/hmiyado/kottage/helper/AuthorizationHelper.kt
+++ src/test/kotlin/com/github/hmiyado/kottage/helper/AuthorizationHelper.kt
@@ -9,9 +9,6 @@ import com.github.hmiyado.kottage.service.users.admins.AdminsService
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.auth.Authentication
-import io.ktor.auth.UserIdPrincipal
-import io.ktor.auth.UserPasswordCredential
-import io.ktor.auth.basic
 import io.ktor.server.testing.TestApplicationRequest
 import io.ktor.sessions.SessionStorage
 import io.ktor.sessions.Sessions
@@ -20,29 +17,9 @@ import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.just
-import java.util.*
 
 class AuthorizationHelper {
     companion object {
-        private const val adminName = "admin"
-        private const val adminPassword = "admin"
-        private val adminBase64: String = Base64.getEncoder().encodeToString("$adminName:$adminPassword".toByteArray())
-        private val authenticationHeaderBasicAdmin = "Basic $adminBase64"
-        val adminCredential = UserPasswordCredential(adminName, adminPassword)
-
-        fun installAuthentication(application: Application) {
-            application.install(Authentication) {
-                basic {
-                    validate { (name, password) ->
-                        if (name == adminName && password == adminName) {
-                            return@validate UserIdPrincipal(adminName)
-                        } else {
-                            null
-                        }
-                    }
-                }
-            }
-        }
 
         fun installSessionAuthentication(
             application: Application,
