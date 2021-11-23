1c5423f2e9ac87030581312e5ea53dc97819795f
miyado <hmiyado@gmail.com>
2021-11-23 18:54:41 +0900

    GET /entries/{serialNumber} becomes OpenApi

--- src/main/kotlin/com/github/hmiyado/kottage/route/entries/EntriesSerialNumberLocation.kt
+++ src/main/kotlin/com/github/hmiyado/kottage/route/entries/EntriesSerialNumberLocation.kt
@@ -9,7 +9,6 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
-import io.ktor.locations.get
 import io.ktor.locations.options
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -19,16 +18,16 @@ import io.ktor.routing.Route
 data class EntriesSerialNumberLocation(val serialNumber: Long) {
     companion object {
         fun addRoute(route: Route, entriesService: EntriesService) = with(route) {
-            get<EntriesSerialNumberLocation> { (serialNumber) ->
-                val entry = entriesService.getEntry(serialNumber)
-                if (entry == null) {
-                    call.respond(HttpStatusCode.NotFound)
-                    return@get
-                }
-                call.respond(entry)
-            }
-
             with(OpenApi) {
+                entriesSerialNumberGet {
+                    val serialNumber = call.entriesSerialNumberGetSerialNumber()
+                    val entry = entriesService.getEntry(serialNumber)
+                    if (entry == null) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@entriesSerialNumberGet
+                    }
+                    call.respond(entry)
+                }
                 entriesSerialNumberPatch { (title, body), user ->
                     val serialNumber = call.entriesSerialNumberPatchSerialNumber()
                     kotlin.runCatching { entriesService.updateEntry(serialNumber, user.id, title, body) }
