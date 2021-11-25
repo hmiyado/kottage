78c18e41f0a0878d33a2fc8463f46139bf61963b
miyado <hmiyado@gmail.com>
2021-11-25 22:51:34 +0900

    update openapi spec

--- api/entry/entryRepository.ts
+++ api/entry/entryRepository.ts
@@ -13,9 +13,14 @@ const getEntries = async () => {
   return KottageClient.entriesGet()
 }
 
+const fetchEntry = async (serialNumber: number) => {
+  return KottageClient.entriesSerialNumberGet({serialNumber})
+}
+
 const EntryRepository = {
   createEntry,
   getEntries,
+  fetchEntry,
 }
 
 export default EntryRepository
