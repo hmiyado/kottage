11e60752d6f9cf9b8a28296ce0a737b7f9cd700b
miyado <hmiyado@gmail.com>
2021-11-25 22:48:59 +0900

    entry page recognize serial number

--- pages/entries/[serialNumber]/index.tsx
+++ pages/entries/[serialNumber]/index.tsx
@@ -1,12 +1,43 @@
+import EntryRepository from 'api/entry/entryRepository'
 import Entry from 'components/entry/entry'
 import TwoColumn from 'components/template/twocolumn/twocolumn'
 
-export default function EntriesSerialNumberPage() {
+export async function getStaticPaths() {
+  const entries = await EntryRepository.getEntries()
+  const serialNumbers = entries.items?.map((v) => {
+    return { params: { serialNumber: v.serialNumber.toString() } }
+  })
+
+  return {
+    paths: serialNumbers ? serialNumbers : [],
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({
+  params,
+}: {
+  params: { serialNumber: number }
+}) {
+  console.log(params.serialNumber)
+
+  return {
+    props: {
+      serialNumber: params.serialNumber,
+    },
+  }
+}
+
+export default function EntriesSerialNumberPage({
+  serialNumber,
+}: {
+  serialNumber: number
+}) {
   return (
     <TwoColumn>
       <>
         <Entry title="title" time="time" author="author">
-          body
+          {serialNumber}
         </Entry>
       </>
     </TwoColumn>
