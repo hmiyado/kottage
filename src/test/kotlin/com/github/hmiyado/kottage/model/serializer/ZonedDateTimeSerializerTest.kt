218bf127a2530730af67516dae100c6221f91990
miyado <hmiyado@gmail.com>
2021-11-28 13:18:37 +0900

    ZonedDateTimeSerializerTest.kt test with ZoneOffset.UTC instead of Asia/Tokyo

--- src/test/kotlin/com/github/hmiyado/kottage/model/serializer/ZonedDateTimeSerializerTest.kt
+++ src/test/kotlin/com/github/hmiyado/kottage/model/serializer/ZonedDateTimeSerializerTest.kt
@@ -3,7 +3,6 @@ package com.github.hmiyado.kottage.model.serializer
 import io.kotest.assertions.json.shouldEqualJson
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import kotlinx.serialization.json.Json
@@ -12,13 +11,13 @@ import kotlinx.serialization.json.JsonPrimitive
 class ZonedDateTimeSerializerTest : DescribeSpec() {
     init {
         describe("ZonedDateTimeSerializer") {
-            it("should encode 2021/05/05 21:12:00 Asia/Tokyo to json") {
-                val date = ZonedDateTime.of(2021, 5, 5, 21, 12, 0, 0, ZoneId.of("Asia/Tokyo"))
-                Json.encodeToString(ZonedDateTimeSerializer, date) shouldEqualJson "\"2021-05-05T21:12:00+0900\""
+            it("should encode 2021/05/05 21:12:00 to json") {
+                val date = ZonedDateTime.of(2021, 5, 5, 21, 12, 0, 0, ZoneOffset.UTC)
+                Json.encodeToString(ZonedDateTimeSerializer, date) shouldEqualJson "\"2021-05-05T21:12:00+0000\""
             }
-            it("should decode 2021/05/05 21:12:00 Asia/Tokyo from json") {
-                val original = "2021-05-05T21:12:00+0900"
-                val expected = ZonedDateTime.of(2021, 5, 5, 21, 12, 0, 0, ZoneOffset.ofHours(9))
+            it("should decode 2021/05/05 21:12:00 from json") {
+                val original = "2021-05-05T21:12:00+0000"
+                val expected = ZonedDateTime.of(2021, 5, 5, 21, 12, 0, 0, ZoneOffset.UTC)
                 Json.decodeFromJsonElement(ZonedDateTimeSerializer, JsonPrimitive(original)) shouldBe expected
             }
             it("should be equal original and value decoded and encoded") {
