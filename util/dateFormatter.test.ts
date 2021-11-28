b988ee0e1e0e18a2b27ed3fedc6a942ebe2111ab
miyado <hmiyado@gmail.com>
2021-11-28 16:42:37 +0900

    entry shows time zone

--- util/dateFormatter.test.ts
+++ util/dateFormatter.test.ts
@@ -1,9 +1,9 @@
 import { dateFormatter } from './dateFormatter'
 
-describe('YYYY-MM-DDThh:mm:ss', () => {
+describe('YYYY-MM-DDThh:mm:ssZ', () => {
   test('should be test', () => {
     const date = new Date(2021, 4, 5, 21, 12, 0, 0)
-    const actual = dateFormatter['YYYY-MM-DDThh:mm:ss'](date)
-    expect(actual).toBe('2021-05-05T21:12:00')
+    const actual = dateFormatter['YYYY-MM-DDThh:mm:ss+0900'](date)
+    expect(actual).toBe('2021-05-05T21:12:00+0900')
   })
 })
