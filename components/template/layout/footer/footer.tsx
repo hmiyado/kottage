085dfaa795c7bcf7aec6effd411cec60ac5ce013
miyado <hmiyado@gmail.com>
2021-12-04 21:55:03 +0900

    share texts in feed and header/footer

--- components/template/layout/footer/footer.tsx
+++ components/template/layout/footer/footer.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import { Constants } from 'util/constants'
 import styles from './footer.module.css'
 
 export default function footer() {
   return (
     <footer className={styles.container}>
-      <div className={styles.text}>(C) 2022 miyado</div>
+      <div className={styles.text}>{Constants.copyright}</div>
 
       <Link href="/contact">
         <a className={styles.link}>contact</a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,12 +20,19 @@ function createAtomFeed(entries: OpenApiEntry[]) {
   const atomFilePath = '/feed/atom.xml'
   const feed = new Feed({
     id: Constants.baseUrl,
+    link: Constants.baseUrl,
     title: Constants.title,
+    description: Constants.description,
     copyright: Constants.copyright,
+    author: { name: Constants.author },
+    favicon: `${Constants.baseUrl}/favicons/favicon-16x16.png`,
     feedLinks: {
       atom: `${Constants.baseUrl}${atomFilePath}`,
     },
   })
+  for (const keyword of Constants.keywords) {
+    feed.addCategory(keyword)
+  }
 
   for (const entry of entries) {
     const url = `${Constants.baseUrl}/entries/${entry.serialNumber}`
