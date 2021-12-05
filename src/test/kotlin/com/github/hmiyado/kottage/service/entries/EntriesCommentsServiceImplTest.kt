0ab18ca3525e3cf7da0d2baa6a2cebb810877c38
miyado <hmiyado@gmail.com>
2021-12-05 22:23:30 +0900

    fix EntriesCommentsServiceImplTest.kt to check entry exists

--- src/test/kotlin/com/github/hmiyado/kottage/service/entries/EntriesCommentsServiceImplTest.kt
+++ src/test/kotlin/com/github/hmiyado/kottage/service/entries/EntriesCommentsServiceImplTest.kt
@@ -1,9 +1,12 @@
 package com.github.hmiyado.kottage.service.entries
 
 import com.github.hmiyado.kottage.model.Comment
+import com.github.hmiyado.kottage.model.Entry
 import com.github.hmiyado.kottage.model.Page
 import com.github.hmiyado.kottage.model.User
 import com.github.hmiyado.kottage.repository.entries.EntryCommentRepository
+import com.github.hmiyado.kottage.repository.entries.EntryRepository
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
@@ -13,13 +16,16 @@ import io.mockk.impl.annotations.MockK
 
 class EntriesCommentsServiceImplTest : DescribeSpec() {
     @MockK
+    lateinit var entryRepository: EntryRepository
+
+    @MockK
     lateinit var entryCommentRepository: EntryCommentRepository
     private lateinit var service: EntriesCommentsService
 
     override fun beforeSpec(spec: Spec) {
         super.beforeSpec(spec)
         MockKAnnotations.init(this, relaxUnitFun = true)
-        service = EntriesCommentsServiceImpl(entryCommentRepository)
+        service = EntriesCommentsServiceImpl(entryRepository, entryCommentRepository)
     }
 
     init {
@@ -27,6 +33,7 @@ class EntriesCommentsServiceImplTest : DescribeSpec() {
             it("should return comments") {
                 val totalCount = 100L
                 val comments = (1..20).map { Comment(id = it.toLong()) }
+                every { entryRepository.getEntry(1) } returns Entry()
                 every { entryCommentRepository.getTotalComments(1) } returns totalCount
                 every { entryCommentRepository.getComments(1, any(), any()) } returns comments
                 val actual = service.getComments(1, null, null)
@@ -37,6 +44,7 @@ class EntriesCommentsServiceImplTest : DescribeSpec() {
                 val limit = 10L
                 val offset = 5L
                 val comments = (1..limit).map { Comment(id = it + offset) }
+                every { entryRepository.getEntry(1) } returns Entry()
                 every { entryCommentRepository.getTotalComments(1) } returns totalCount
                 every { entryCommentRepository.getComments(1, limit, offset) } returns comments
                 val actual = service.getComments(1, limit, offset)
@@ -47,6 +55,7 @@ class EntriesCommentsServiceImplTest : DescribeSpec() {
                 val limit = 10000L
                 val offset = 5L
                 val comments = (1..20).map { Comment(id = it + offset) }
+                every { entryRepository.getEntry(1) } returns Entry()
                 every { entryCommentRepository.getTotalComments(1) } returns totalCount
                 every {
                     entryCommentRepository.getComments(
@@ -58,6 +67,10 @@ class EntriesCommentsServiceImplTest : DescribeSpec() {
                 val actual = service.getComments(1, limit, offset)
                 actual shouldBe Page(totalCount, comments)
             }
+            it("should throw not found entry exception") {
+                every { entryRepository.getEntry(any()) } returns null
+                shouldThrow<EntriesService.NoSuchEntryException> { service.getComments(100, null, null) }
+            }
         }
 
         describe("addComment") {
@@ -65,9 +78,14 @@ class EntriesCommentsServiceImplTest : DescribeSpec() {
                 val body = "body"
                 val user = User()
                 val expected = Comment(body = body, author = user)
+                every { entryRepository.getEntry(1) } returns Entry()
                 every { entryCommentRepository.createComment(1, body, user.id) } returns expected
                 val actual = service.addComment(1, body, user)
                 actual shouldBe expected
+            }
+            it("should throw not found entry exception") {
+                every { entryRepository.getEntry(any()) } returns null
+                shouldThrow<EntriesService.NoSuchEntryException> { service.addComment(100, "body", User()) }
             }
         }
     }
