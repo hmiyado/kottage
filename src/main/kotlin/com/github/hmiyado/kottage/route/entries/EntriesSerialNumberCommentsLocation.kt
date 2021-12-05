1f965ba079e24401afc1f408b6a47cb4bc2a0b19
miyado <hmiyado@gmail.com>
2021-12-05 22:36:26 +0900

    fix RoutingTest and add OPTIONS /entries/{serialNumber}/comments

--- src/main/kotlin/com/github/hmiyado/kottage/route/entries/EntriesSerialNumberCommentsLocation.kt
+++ src/main/kotlin/com/github/hmiyado/kottage/route/entries/EntriesSerialNumberCommentsLocation.kt
@@ -2,14 +2,18 @@ package com.github.hmiyado.kottage.route.entries
 
 import com.github.hmiyado.kottage.model.Comment
 import com.github.hmiyado.kottage.model.Page
+import com.github.hmiyado.kottage.openapi.Paths
 import com.github.hmiyado.kottage.openapi.apis.OpenApi
 import com.github.hmiyado.kottage.openapi.models.Comments
+import com.github.hmiyado.kottage.route.allowMethods
 import com.github.hmiyado.kottage.route.users.UsersLocation
 import com.github.hmiyado.kottage.service.entries.EntriesCommentsService
 import io.ktor.application.call
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import io.ktor.routing.options
 import com.github.hmiyado.kottage.openapi.models.Comment as OpenApiComment
 
 class EntriesSerialNumberCommentsLocation(
@@ -30,6 +34,10 @@ class EntriesSerialNumberCommentsLocation(
                 val comment = entriesCommentsService.addComment(serialNumber, body, user)
                 call.respond(HttpStatusCode.Created, comment.toOpenApiComment())
             }
+        }
+
+        options(Paths.entriesSerialNumberCommentsGet) {
+            call.response.allowMethods(HttpMethod.Options, HttpMethod.Get, HttpMethod.Post)
         }
     }
 }
