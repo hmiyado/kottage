bf8ac6c440af1404ea36ebf69f8760bcd82b80fc
miyado <hmiyado@gmail.com>
2021-12-13 22:31:39 +0900

    use rss link

--- components/sidemenu/servicereference/servicereference.tsx
+++ components/sidemenu/servicereference/servicereference.tsx
@@ -1,11 +1,12 @@
 import styles from './servicereference.module.css'
 import Image from 'next/image'
-import Script from 'next/script'
 import GitHubOnLightSurface from '../../../public/components/sidemenu/servicereference/GitHub-Mark-64px.png'
 import GitHubOnDarkSurface from '../../../public/components/sidemenu/servicereference/GitHub-Mark-Light-64px.png'
 import Qiita from '../../../public/components/sidemenu/servicereference/qiita.png'
 import TwitterOnLightSurface from '../../../public/components/sidemenu/servicereference/twitter-on-light.png'
 import TwitterOnDarkSurface from '../../../public/components/sidemenu/servicereference/twitter-on-dark.png'
+import RssOnLightSurface from '../../../public/components/sidemenu/servicereference/rss-on-light.png'
+import RssOnDarkSurface from '../../../public/components/sidemenu/servicereference/rss-on-dark.png'
 
 const services = [
   {
@@ -25,6 +26,12 @@ const services = [
     url: 'https://twitter.com/miyado20th',
     lightImage: TwitterOnLightSurface,
     darkImage: TwitterOnDarkSurface,
+  },
+  {
+    name: 'Feed',
+    url: 'https://miyado.dev/feed/atom.xml',
+    lightImage: RssOnLightSurface,
+    darkImage: RssOnDarkSurface,
   },
 ]
 
