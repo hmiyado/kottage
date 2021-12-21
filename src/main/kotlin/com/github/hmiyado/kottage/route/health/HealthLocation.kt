2546192f477ff82eae98a088403241bc9ccf5378
miyado <hmiyado@gmail.com>
2021-12-21 09:54:07 +0900

    GET /health becomes OpenApi

--- src/main/kotlin/com/github/hmiyado/kottage/route/health/HealthLocation.kt
+++ src/main/kotlin/com/github/hmiyado/kottage/route/health/HealthLocation.kt
@@ -1,23 +1,26 @@
 package com.github.hmiyado.kottage.route.health
 
-import com.github.hmiyado.kottage.route.Path
+import com.github.hmiyado.kottage.model.toOpenApiHealth
+import com.github.hmiyado.kottage.openapi.Paths
+import com.github.hmiyado.kottage.openapi.apis.OpenApi
 import com.github.hmiyado.kottage.route.allowMethods
 import com.github.hmiyado.kottage.service.health.HealthService
 import io.ktor.application.call
 import io.ktor.http.HttpMethod
 import io.ktor.response.respond
 import io.ktor.routing.Route
-import io.ktor.routing.get
 import io.ktor.routing.options
 
 class HealthLocation(
     private val healthService: HealthService
 ) {
     fun addRoute(route: Route) = with(route) {
-        get(Path.Health) {
-            call.respond(healthService.getHealth())
+        with(OpenApi) {
+            healthGet {
+                call.respond(healthService.getHealth().toOpenApiHealth())
+            }
         }
-        options(Path.Health) {
+        options(Paths.healthGet) {
             call.response.allowMethods(HttpMethod.Options, HttpMethod.Get)
         }
     }
