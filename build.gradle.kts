1629cef3446986d95a24671758dc4714d0d1f500
hmiyado <hmiyado@gmail.com>
2021-12-26 09:32:48 +0000

    version bump to v1-202112260932

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "kottage"
-version = "v1-202112260237"
+version = "v1-202112260932"
 
 val generatedSourcePath = buildDir.resolve(File("generated/src/main/kotlin"))
 tasks.register("generateBuildConfig") {
