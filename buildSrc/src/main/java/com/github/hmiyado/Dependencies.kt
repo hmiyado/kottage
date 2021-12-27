84a1f5952014e4c2d523983e976a5657c932ceed
miyado <hmiyado@gmail.com>
2021-12-27 15:24:10 +0900

    update Ktor version to 1.6.7

--- buildSrc/src/main/java/com/github/hmiyado/Dependencies.kt
+++ buildSrc/src/main/java/com/github/hmiyado/Dependencies.kt
@@ -11,7 +11,7 @@ object Dependencies {
     }
 
     object Ktor {
-        private const val version = "1.6.5"
+        private const val version = "1.6.7"
         const val serverNetty = "io.ktor:ktor-server-netty:$version"
         const val serialization = "io.ktor:ktor-serialization:$version"
         const val test = "io.ktor:ktor-server-tests:$version"
