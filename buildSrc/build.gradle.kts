2069d9acf68f6f7d2f5d5146be321a2658689b42
miyado <hmiyado@gmail.com>
2021-12-27 15:28:33 +0900

    use jvm target 11 in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,12 @@ repositories {
     mavenCentral()
 }
 
+val compileKotlin by tasks.getting(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
 dependencies {
     implementation("com.squareup:kotlinpoet:1.10.2")
 }
