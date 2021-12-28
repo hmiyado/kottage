04a0e14616b81729f9ab06f661e784dbe6fcbc3c
hmiyado <hmiyado@gmail.com>
2021-12-28 06:50:31 +0000

    version bump to v1-202112280650

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "kottage"
-version = "v1-202112280421"
+version = "v1-202112280650"
 
 val generatedSourcePath = buildDir.resolve(File("generated/src/main/kotlin"))
 tasks.register("generateBuildConfig") {
--- infra/variables.tf
+++ infra/variables.tf
@@ -35,7 +35,7 @@ variable "vercel_deploy_hook" {
 
 variable "kottage_image" {
   type    = string
-  default = "miyado/kottage:v1-202112280421"
+  default = "miyado/kottage:v1-202112280650"
 }
 
 variable "ecs_cloudwatch_kottage_api" {
