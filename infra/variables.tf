9a606cde8ccf9ebd945f73428fde6eac682041ce
hmiyado <hmiyado@gmail.com>
2021-12-29 13:42:02 +0000

    version bump to v1-202112291342

--- infra/variables.tf
+++ infra/variables.tf
@@ -35,7 +35,7 @@ variable "vercel_deploy_hook" {
 
 variable "kottage_image" {
   type    = string
-  default = "miyado/kottage:v1-202112281256"
+  default = "miyado/kottage:v1-202112291342"
 }
 
 variable "ecs_cloudwatch_kottage_api" {
