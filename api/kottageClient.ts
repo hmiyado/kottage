ec71a878d92192fa82974109fcb124d7bd2e1c6c
miyado <hmiyado@gmail.com>
2021-12-29 22:12:06 +0900

    extract CSRF token from header

--- api/kottageClient.ts
+++ api/kottageClient.ts
@@ -1,4 +1,41 @@
-import { Configuration, DefaultApi } from './openapi/generated'
+import {
+  Configuration,
+  DefaultApi,
+  Middleware,
+  RequestContext,
+  ResponseContext,
+  FetchParams,
+} from './openapi/generated'
+
+class CsrfTokenMiddleware implements Middleware {
+  private CsrfTokenHeaderKey = 'X-CSRF-Token'
+  private csrfToken: string | null = null
+
+  pre(context: RequestContext): Promise<FetchParams | void> {
+    const currentCsrfToken = this.csrfToken
+    if (currentCsrfToken == null) {
+      return Promise.resolve()
+    }
+
+    const { url, init } = context
+    const headers = Object.assign(init.headers ?? {}, {
+      [this.CsrfTokenHeaderKey]: this.csrfToken,
+    })
+    return Promise.resolve({
+      url,
+      init: {
+        ...init,
+        headers,
+      },
+    })
+  }
+
+  post(context: ResponseContext): Promise<Response | void> {
+    const { response } = context
+    this.csrfToken = response.headers.get(this.CsrfTokenHeaderKey)
+    return Promise.resolve()
+  }
+}
 
 class OpenApi extends DefaultApi {
   constructor() {
@@ -6,6 +43,7 @@ class OpenApi extends DefaultApi {
       new Configuration({
         basePath: process.env.NEXT_PUBLIC_KOTTAGE_BASE_PATH,
         credentials: 'include',
+        middleware: [new CsrfTokenMiddleware()],
       })
     )
   }
