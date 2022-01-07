d17c16205c92935ffd66420f6a655293c9ad61a7
miyado <hmiyado@gmail.com>
2022-01-07 09:37:56 +0900

    create Csrf and OpenApi status page router

--- src/main/kotlin/com/github/hmiyado/kottage/application/plugins/statuspages/StatusPages.kt
+++ src/main/kotlin/com/github/hmiyado/kottage/application/plugins/statuspages/StatusPages.kt
@@ -1,40 +1,16 @@
 package com.github.hmiyado.kottage.application.plugins.statuspages
 
-import com.github.hmiyado.kottage.application.plugins.CustomHeaders
-import com.github.hmiyado.kottage.application.plugins.csrf.CsrfHeaderException
-import com.github.hmiyado.kottage.application.plugins.csrf.CsrfOriginException
-import com.github.hmiyado.kottage.application.plugins.csrf.CsrfTokenException
-import com.github.hmiyado.kottage.openapi.apis.OpenApi
 import com.github.hmiyado.kottage.route.entries.EntriesSerialNumberCommentsCommentIdLocation
 import com.github.hmiyado.kottage.route.entries.EntriesSerialNumberLocation
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.StatusPages
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respond
 
 fun Application.statusPages() {
     install(StatusPages) {
-        exception<OpenApi.PathParameterUnrecognizableException> { cause ->
-            call.respond(HttpStatusCode.BadRequest, cause.message ?: "path parameter is not valid")
-        }
+        OpenApiStatusPageRouter.addStatusPage(this)
 
-        exception<OpenApi.RequestBodyUnrecognizableException> { cause ->
-            call.respond(HttpStatusCode.BadRequest, cause.message ?: "request body should be json")
-        }
-
-        exception<CsrfTokenException> {
-            call.respond(HttpStatusCode.Forbidden, "Csrf Token is invalid")
-        }
-
-        exception<CsrfHeaderException> {
-            call.respond(HttpStatusCode.Forbidden, "Csrf Header ${CustomHeaders.XCSRFToken} is required")
-        }
-
-        exception<CsrfOriginException> {
-            call.respond(HttpStatusCode.Forbidden, "Origin is invalid")
-        }
+        CsrfStatusPageRouter.addStatusPage(this)
 
         EntriesSerialNumberLocation.addStatusPage(this)
 
