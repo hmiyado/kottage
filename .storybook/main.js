50756ac41ca52f999ab0818b5eab96588f32dba5
miyado <hmiyado@gmail.com>
2022-01-15 20:30:17 +0900

    fix warning of static folder for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  staticDirs: ['../public'],
   stories: [
     './resources/**/*.stories.mdx',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
