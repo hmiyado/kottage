d360255cbd14024566229e38a85d87781b7e3fb0
miyado <hmiyado@gmail.com>
2022-01-23 17:14:41 +0900

    merge settings for storybook decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,13 +3,9 @@ import { useState } from 'react'
 import UserContext from '../context/user'
 import * as nextImage from 'next/image'
 import { initialize, mswDecorator } from 'msw-storybook-addon'
-import { addDecorator } from '@storybook/react'
 
 // Initialize MSW
 initialize()
-
-// Provide the MSW addon decorator globally
-export const decorators = [mswDecorator]
 
 Object.defineProperty(nextImage, 'default', {
   configurable: true,
@@ -26,20 +22,22 @@ export const parameters = {
   },
 }
 
-const decorator = (storyFn) => {
-  const [user, updateUser] = useState(null)
+export const decorators = [
+  (storyFn) => {
+    const [user, updateUser] = useState(null)
 
-  return (
-    <UserContext.Provider
-      value={{
-        user,
-        updateUser: (newUser) => {
-          updateUser(newUser)
-        },
-      }}
-    >
-      {storyFn()}
-    </UserContext.Provider>
-  )
-}
-addDecorator(decorator)
+    return (
+      <UserContext.Provider
+        value={{
+          user,
+          updateUser: (newUser) => {
+            updateUser(newUser)
+          },
+        }}
+      >
+        {storyFn()}
+      </UserContext.Provider>
+    )
+  },
+  mswDecorator,
+]
