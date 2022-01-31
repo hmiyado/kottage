db1e4a9bdb0adf78c3d8652aa934045ff16156f2
miyado <hmiyado@gmail.com>
2022-01-31 09:47:36 +0900

    prettier before check mockServiceWorker.js diff

--- scripts/update-msw.sh
+++ scripts/update-msw.sh
@@ -1,6 +1,6 @@
 # public/mockServiceWorker.js had diff
 # https://github.com/hmiyado/kottage-front/runs/4930491171?check_suite_focus=true
-
+yarn prettier --write --ignore-unknown "public/**/*"
 if [ "$(git diff --name-only)" = public/mockServiceWorker.js ]; then
   git add public/mockServiceWorker.js
   git commit -m 'update public/mockServiceWorker.js'
