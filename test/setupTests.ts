9e4425023da0baf9dfdeda05347feae8725a05a6
miyado <hmiyado@gmail.com>
2022-03-14 19:31:54 +0900

    fix to pass test

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -7,6 +7,10 @@ if (!globalThis.fetch) {
   globalThis.fetch = fetch as any
 }
 
+// https://github.com/facebook/jest/issues/9983#issuecomment-696427273
+import { TextDecoder } from 'util'
+global.TextDecoder = TextDecoder as any
+
 // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
