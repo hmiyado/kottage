6885ef41c86a8ac4663a62aba6f2b1a5dfaa2f91
miyado <hmiyado@gmail.com>
2022-03-15 22:27:07 +0900

    update patch-package on github action

--- scripts/update-patch-package.sh
+++ scripts/update-patch-package.sh
@@ -5,6 +5,9 @@ fi
 
 yarn patch-package next
 if [ -n "$(git diff --name-only)"]; then
+  if [ -n $SOURCE_BRANCH ]; then
+    git checkout $SOURCE_BRANCH
+  fi
   git add -A
   git commit -m 'update patch-package next'
   git push
