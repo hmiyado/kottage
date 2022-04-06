edaa1a1f233a5e15f865e1123d6a1ca3cfacd7f7
hmiyado <hmiyado@gmail.com>
2022-04-06 14:58:34 +0000

    version bump to v1-202204061458

--- infra/variables.tf
+++ infra/variables.tf
@@ -35,7 +35,7 @@ variable "vercel_deploy_hook" {
 
 variable "kottage_image" {
   type    = string
-  default = "miyado/kottage:v1-202204060039"
+  default = "miyado/kottage:v1-202204061458"
 }
 
 variable "main_availability_zones" {
