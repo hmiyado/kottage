abe50791d20eb812ea0fb63e8bb4b501d764a386
miyado <hmiyado@gmail.com>
2022-05-15 11:28:32 +0900

    extract swr hooks as custom hook

--- components/plurals/sidemenu/userform/userform.tsx
+++ components/plurals/sidemenu/userform/userform.tsx
@@ -1,86 +1,24 @@
 import UserContext from 'context/user'
-import { useContext, useEffect, useState } from 'react'
-import useSWR, { useSWRConfig } from 'swr'
-import UserRepository from 'repository/user/userRepository'
+import { useContext, useState } from 'react'
 import SignInForm from './signinform/signinform'
 import SignOutForm from './signoutform/signoutform'
-
-type NextAction =
-  | {
-      type: 'signIn' | 'signUp'
-      id: string
-      password: string
-    }
-  | { type: 'signOut' }
-  | { type: 'currentUser' }
+import {
+  useCurrentUser,
+  UserHookAction,
+  useSignIn,
+  useSignOut,
+  useSignUp,
+} from './userformHooks'
 
 export default function UserForm(): JSX.Element {
-  const [nextAction, setNextAction] = useState<NextAction>({
+  const [nextAction, setNextAction] = useState<UserHookAction>({
     type: 'currentUser',
   })
-  const shouldSignUp = nextAction.type === 'signUp'
-  const shouldSignIn = nextAction.type === 'signIn'
-  const shouldSignOut = nextAction.type === 'signOut'
-  const { user, updateUser } = useContext(UserContext)
-  const { mutate } = useSWRConfig()
-  const { error: signOutError } = useSWR(
-    shouldSignOut ? 'signOut' : null,
-    UserRepository.signOut
-  )
-  const { data: currentUser } = useSWR(
-    nextAction.type === 'currentUser' ? 'currentUser' : null,
-    UserRepository.current,
-    {
-      shouldRetryOnError: false,
-      fallbackData: null,
-    }
-  )
-  const { data: signInUser, error: signInError } = useSWR(
-    shouldSignIn ? nextAction : null,
-    ({ id, password }) => UserRepository.signIn(id, password),
-    {
-      shouldRetryOnError: false,
-    }
-  )
-  const { data: signUpUser, error: signUpError } = useSWR(
-    shouldSignUp ? nextAction : null,
-    ({ id, password }) => UserRepository.signUp(id, password),
-    {
-      shouldRetryOnError: false,
-    }
-  )
-  useEffect(() => {
-    if (signUpUser && !signUpError) {
-      setNextAction({ type: 'currentUser' })
-      mutate('currentUser', signUpUser)
-      updateUser(signUpUser)
-      return
-    }
-    if (signInUser && !signInError) {
-      setNextAction({ type: 'currentUser' })
-      mutate('currentUser', signInUser)
-      updateUser(signInUser)
-      return
-    }
-    if (shouldSignOut && !signOutError) {
-      setNextAction({ type: 'currentUser' })
-      mutate('currentUser', null)
-      updateUser(null)
-      return
-    }
-    updateUser(currentUser || null)
-  }, [
-    shouldSignOut,
-    mutate,
-    updateUser,
-    currentUser,
-    signOutError,
-    signInUser,
-    nextAction,
-    signInError,
-    signUpUser,
-    signUpError,
-  ])
+  const { user } = useContext(UserContext)
+  useCurrentUser(nextAction)
+  useSignUp(nextAction, setNextAction)
+  useSignIn(nextAction, setNextAction)
+  useSignOut(nextAction, setNextAction)
 
   return user?.screenName ? (
     <SignOutForm
---
+++ components/plurals/sidemenu/userform/userformHooks.tsx
@@ -0,0 +1,93 @@
+import UserContext from 'context/user'
+import { useContext, useEffect } from 'react'
+import UserRepository from 'repository/user/userRepository'
+import useSWR, { useSWRConfig } from 'swr'
+
+export type UserHookAction =
+  | { type: 'currentUser' }
+  | {
+      type: 'signIn' | 'signUp'
+      id: string
+      password: string
+    }
+  | { type: 'signOut' }
+
+export function useCurrentUser(action: UserHookAction) {
+  const { data: currentUser } = useSWR(
+    action.type === 'currentUser' ? 'currentUser' : null,
+    UserRepository.current,
+    {
+      shouldRetryOnError: false,
+      fallbackData: null,
+    }
+  )
+
+  const { updateUser } = useContext(UserContext)
+  useEffect(() => {
+    updateUser(currentUser || null)
+  })
+}
+
+export function useSignUp(
+  action: UserHookAction,
+  setNextAction: (action: UserHookAction) => void
+) {
+  const { data: signUpUser, error: signUpError } = useSWR(
+    action.type === 'signUp' ? action : null,
+    ({ id, password }) => UserRepository.signUp(id, password),
+    {
+      shouldRetryOnError: false,
+    }
+  )
+
+  const { updateUser } = useContext(UserContext)
+  const { mutate } = useSWRConfig()
+  useEffect(() => {
+    if (signUpUser && !signUpError) {
+      setNextAction({ type: 'currentUser' })
+      mutate('currentUser', signUpUser)
+      updateUser(signUpUser)
+    }
+  })
+}
+
+export function useSignIn(
+  action: UserHookAction,
+  setNextAction: (action: UserHookAction) => void
+) {
+  const { data: signInUser, error: signInError } = useSWR(
+    action.type === 'signIn' ? action : null,
+    ({ id, password }) => UserRepository.signIn(id, password),
+    {
+      shouldRetryOnError: false,
+    }
+  )
+  const { updateUser } = useContext(UserContext)
+  const { mutate } = useSWRConfig()
+  useEffect(() => {
+    if (signInUser && !signInError) {
+      setNextAction({ type: 'currentUser' })
+      mutate('currentUser', signInUser)
+      updateUser(signInUser)
+    }
+  })
+}
+
+export function useSignOut(
+  action: UserHookAction,
+  setNextAction: (action: UserHookAction) => void
+) {
+  const { data: signOut, error: signOutError } = useSWR(
+    action.type === 'signOut' ? 'signOut' : null,
+    UserRepository.signOut
+  )
+  const { updateUser } = useContext(UserContext)
+  const { mutate } = useSWRConfig()
+  useEffect(() => {
+    if (signOut && !signOutError) {
+      setNextAction({ type: 'currentUser' })
+      mutate('currentUser', null)
+      updateUser(null)
+    }
+  })
+}
