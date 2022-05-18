0da494f254bf3b4ce7f767207dbc3086a7cce1d1
miyado <hmiyado@gmail.com>
2022-05-18 22:57:06 +0900

    switch to source branch when update msw

--- scripts/update-msw.sh
+++ scripts/update-msw.sh
@@ -2,6 +2,10 @@
 # https://github.com/hmiyado/kottage-front/runs/4930491171?check_suite_focus=true
 yarn prettier --write --ignore-unknown "public/**/*"
 if [ "$(git diff --name-only)" = public/mockServiceWorker.js ]; then
+  if [ -n $SOURCE_BRANCH ]; then
+    git fetch origin $SOURCE_BRANCH
+    git switch $SOURCE_BRANCH
+  fi
   git add public/mockServiceWorker.js
   git commit -m 'update public/mockServiceWorker.js'
   git push
