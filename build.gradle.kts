acee43a90067bcaa76b71a730c38c7b8650cc414
hmiyado <hmiyado@gmail.com>
2022-06-08 13:56:20 +0000

    version bump to v1-202206081356

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "kottage"
-version = "v1-202205290746"
+version = "v1-202206081356"
 
 val generatedSourcePath = buildDir.resolve(File("generated/src/main/kotlin"))
 tasks.register("generateBuildConfig") {
--- infra/variables.tf
+++ infra/variables.tf
@@ -35,7 +35,7 @@ variable "vercel_deploy_hook" {
 
 variable "kottage_image" {
   type    = string
-  default = "miyado/kottage:v1-202205290746"
+  default = "miyado/kottage:v1-202206081356"
 }
 
 variable "main_availability_zones" {
