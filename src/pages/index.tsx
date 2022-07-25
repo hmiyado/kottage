d20de44a229f2e73ca1e8948992887b671fb418d
miyado <10195648+hmiyado@users.noreply.github.com>
2022-07-25 21:49:51 +0900

    Move atom.xml from public to dynamic resource

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,55 +4,12 @@ import {
   EntryProps,
 } from '../components/plurals/entry/entry'
 import { entryPerPage, getPageCount } from './pages/[currentPage]'
-import { Entry as OpenApiEntry } from 'repository/openapi/generated/models'
-import { Feed } from 'feed'
-import fs from 'fs'
-import { Constants } from 'util/constants'
 import Root from 'components/presentation/root/root'
-
-function createAtomFeed(entries: OpenApiEntry[]) {
-  const atomFilePath = '/feed/atom.xml'
-  const feed = new Feed({
-    id: Constants.baseUrl,
-    link: Constants.baseUrl,
-    title: Constants.title,
-    description: Constants.description,
-    copyright: Constants.copyright,
-    author: { name: Constants.author },
-    favicon: `${Constants.baseUrl}/favicons/favicon-16x16.png`,
-    feedLinks: {
-      atom: `${Constants.baseUrl}${atomFilePath}`,
-    },
-  })
-  for (const keyword of Constants.keywords) {
-    feed.addCategory(keyword)
-  }
-
-  for (const entry of entries) {
-    const url = `${Constants.baseUrl}/entries/${entry.serialNumber}`
-    feed.addItem({
-      title: entry.title,
-      id: url,
-      link: url,
-      content: entry.body,
-      date: entry.dateTime,
-      author: [
-        {
-          name: entry.author.screenName,
-        },
-      ],
-    })
-  }
-
-  fs.mkdirSync('../src/public/feed', { recursive: true })
-  fs.writeFileSync(`../src/public${atomFilePath}`, feed.atom1())
-}
 
 export async function getStaticProps() {
   try {
     const openapiEntries = await EntryRepository.getEntries(entryPerPage)
     const pageCount = getPageCount(openapiEntries.totalCount)
-    createAtomFeed(openapiEntries.items)
     const entries = openapiEntries.items
       ?.map((v) => {
         return convertEntryToProps(v)
