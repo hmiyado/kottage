430a1c51b8386abcbce50668181b572beab78f43
miyado <10195648+hmiyado@users.noreply.github.com>
2022-07-25 21:55:22 +0900

    Return feed url as localhost in development

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,5 +1,8 @@
 export const Constants = {
-  baseUrl: 'https://miyado.dev',
+  baseUrl:
+    process.env.NODE_ENV == 'production'
+      ? 'https://miyado.dev'
+      : 'http://localhost:3000',
   title: 'miyado.dev',
   copyright: `(C) ${new Date().getFullYear()} miyado`,
   description: 'miyado の日常とときたま開発',
