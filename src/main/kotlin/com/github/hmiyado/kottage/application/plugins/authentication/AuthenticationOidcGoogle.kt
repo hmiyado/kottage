11b219de4b0e677c2b569d64d1bab9ac032f7bc0
miyado <10195648+hmiyado@users.noreply.github.com>
2023-01-04 22:14:23 +0900

    Use discovery document for google oauth2.0 endpoint

--- src/main/kotlin/com/github/hmiyado/kottage/application/plugins/authentication/AuthenticationOidcGoogle.kt
+++ src/main/kotlin/com/github/hmiyado/kottage/application/plugins/authentication/AuthenticationOidcGoogle.kt
@@ -1,22 +1,32 @@
 package com.github.hmiyado.kottage.application.plugins.authentication
 
 import com.github.hmiyado.kottage.application.configuration.OauthGoogle
+import com.github.hmiyado.kottage.repository.oauth.OauthGoogleRepository
 import io.ktor.client.HttpClient
 import io.ktor.http.HttpMethod
 import io.ktor.server.auth.AuthenticationConfig
 import io.ktor.server.auth.OAuthServerSettings
 import io.ktor.server.auth.oauth
 import io.ktor.util.GenerateOnlyNonceManager
+import kotlinx.coroutines.runBlocking
 
-fun AuthenticationConfig.oidcGoogle(httpClient: HttpClient, oauthGoogle: OauthGoogle, redirects: MutableMap<String, String>) {
+fun AuthenticationConfig.oidcGoogle(
+    httpClient: HttpClient,
+    oauthGoogle: OauthGoogle,
+    oauthGoogleRepository: OauthGoogleRepository,
+    redirects: MutableMap<String, String>,
+) {
     oauth("oidc-google") {
+        val config = runBlocking {
+            oauthGoogleRepository.getConfig()
+        }
         // todo: distinct dev and production host
         urlProvider = { "http://localhost:8080/oauth/google/callback" }
         providerLookup = {
             OAuthServerSettings.OAuth2ServerSettings(
                 name = "google",
-                authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
+                authorizeUrl = config.authorizationEndpoint,
+                accessTokenUrl = config.tokenEndpoint,
                 requestMethod = HttpMethod.Post,
                 clientId = oauthGoogle.clientId,
                 clientSecret = oauthGoogle.clientSecret,
