e889a5607409e80af16d3e24582363dc42da9ef2
miyado <10195648+hmiyado@users.noreply.github.com>
2023-01-10 22:09:53 +0900

    Fix tests

--- src/components/plurals/sidemenu/userform/userform.stories.tsx
+++ src/components/plurals/sidemenu/userform/userform.stories.tsx
@@ -4,6 +4,7 @@ import UserForm from './userform'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { compose, rest } from 'msw'
 import { SWRConfig } from 'swr'
+import { Constants } from 'util/constants'
 
 export default {
   title: 'sidemenu/UserForm',
@@ -34,7 +35,7 @@ SignIn.parameters = {
   msw: {
     handlers: [
       rest.get(
-        `http://localhost:8080/api/v1/users/current`,
+        `${Constants.backendUrl}/api/v1/users/current`,
         (req, res, ctx) => {
           return res(compose(ctx.status(401)), ctx.json({}))
         }
@@ -48,7 +49,7 @@ SignOut.parameters = {
   msw: {
     handlers: [
       rest.get(
-        `http://localhost:8080/api/v1/users/current`,
+        `${Constants.backendUrl}/api/v1/users/current`,
         (req, res, ctx) => {
           return res(
             ctx.json({
