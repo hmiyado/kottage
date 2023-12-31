80e01a77c6003a0ae14526196b6040bd665a7f9b
miyado <10195648+hmiyado@users.noreply.github.com>
2023-12-31 17:07:31 +0900

    Maybe run any environment with various timezone

--- src/util/dateFormatter.test.ts
+++ src/util/dateFormatter.test.ts
@@ -2,8 +2,8 @@ import { dateFormatter } from './dateFormatter'
 
 describe('YYYY-MM-DDThh:mm:ssZ', () => {
   test('should be test', () => {
-    const date = new Date('2021-05-05T21:12:00+09:00')
+    const date = new Date('2021-05-05T21:12:00+10:00')
     const actual = dateFormatter['YYYY-MM-DDThh:mm:ss+09:00'](date)
-    expect(actual).toBe('2021-05-05T21:12:00+09:00')
+    expect(actual).toBe('2021-05-05T20:12:00+09:00')
   })
 })
