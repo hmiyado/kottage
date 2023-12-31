08da3a122e8a285a05092ba8ef91d49d0e5a356a
miyado <10195648+hmiyado@users.noreply.github.com>
2023-12-31 16:51:52 +0900

    Fix a test for Date constructor to include timezone

--- src/util/dateFormatter.test.ts
+++ src/util/dateFormatter.test.ts
@@ -2,7 +2,7 @@ import { dateFormatter } from './dateFormatter'
 
 describe('YYYY-MM-DDThh:mm:ssZ', () => {
   test('should be test', () => {
-    const date = new Date(2021, 4, 5, 21, 12, 0, 0)
+    const date = new Date('2021-05-05T21:12:00+09:00')
     const actual = dateFormatter['YYYY-MM-DDThh:mm:ss+09:00'](date)
     expect(actual).toBe('2021-05-05T21:12:00+09:00')
   })
