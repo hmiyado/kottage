8c7599c133d8fecff2ed797b5ef31386c6b10b10
miyado <10195648+hmiyado@users.noreply.github.com>
2025-08-24 15:27:14 +0900

    Remove submodule references and update paths in openapitools.json and package.json

--- frontend/scripts/submodule/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git submodule update --init --recursive
-git -C kottage config core.sparsecheckout true
-cp scripts/submodule/sparse-checkout .git/modules/kottage/info/sparse-checkout
-git -C kottage read-tree -mu HEAD
