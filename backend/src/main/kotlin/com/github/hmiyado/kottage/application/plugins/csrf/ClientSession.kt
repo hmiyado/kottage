893fee3126ab615af3abee4cafa4ef11d2a273e5
miyado <10195648+hmiyado@users.noreply.github.com>
2025-09-07 19:07:15 +0900

    Add serialization support to ClientSession data class

--- backend/src/main/kotlin/com/github/hmiyado/kottage/application/plugins/csrf/ClientSession.kt
+++ backend/src/main/kotlin/com/github/hmiyado/kottage/application/plugins/csrf/ClientSession.kt
@@ -1,7 +1,9 @@
 package com.github.hmiyado.kottage.application.plugins.csrf
 
 import io.github.hmiyado.ktor.csrfprotection.CsrfTokenBoundClient
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ClientSession(
     val token: String,
 ) : CsrfTokenBoundClient {
