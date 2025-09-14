47963e32c26e57840fdc48eb13a92dde0c70e647
miyado <10195648+hmiyado@users.noreply.github.com>
2025-09-14 12:28:37 +0900

    Refactor CSRF installation to remove origin checks and enhance custom header validation

--- backend/src/main/kotlin/com/github/hmiyado/kottage/application/plugins/csrf/InstallCsrf.kt
+++ backend/src/main/kotlin/com/github/hmiyado/kottage/application/plugins/csrf/InstallCsrf.kt
@@ -1,36 +1,41 @@
 package com.github.hmiyado.kottage.application.plugins.csrf
 
-import com.github.hmiyado.kottage.application.configuration.DevelopmentConfiguration
 import com.github.hmiyado.kottage.application.plugins.CustomHeaders
+import com.github.hmiyado.kottage.application.plugins.statuspages.ErrorFactory
+import com.github.hmiyado.kottage.openapi.models.Error403Cause
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.csrf.CSRF
-import io.ktor.server.plugins.origin
-import org.koin.ktor.ext.get
+import io.ktor.server.response.respond
+import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
 
 fun Application.csrf() {
     install(CSRF) {
-        // tests Origin is an expected value
-        val origin = if (this@csrf.get<DevelopmentConfiguration>() == DevelopmentConfiguration.Production) {
-            "https://miyado.dev"
-        } else {
-            "http://localhost:3000"
-        }
-        allowOrigin(origin)
-
-        // tests Origin matches Host header
-        originMatchesHost()
+        // CORS でも Origin のチェックをしているので、CSRF ではチェックしない
+        // allowOrigin(origin)
 
         // custom header checks
-        checkHeader(CustomHeaders.XCSRFToken.uppercase())
+        checkHeader(CustomHeaders.XCSRFToken) { headerValue ->
+            // ClientSession をとってくる
+            // ClientSession に紐づく CSRF トークンを取得する
+            // 一致するかどうかを比較する
+            val clientSession = sessions.get<ClientSession>() ?: return@checkHeader false
+            val csrfTokenSession = sessions.get<CsrfTokenSession>() ?: return@checkHeader false
+            return@checkHeader csrfTokenSession.clientSession == clientSession
+        }
 
-        onFailure {
-            if(!request.headers.contains(CustomHeaders.XCSRFToken.uppercase())) {
-                throw CsrfHeaderException()
-            }
-            if(request.origin.uri != origin) {
-                throw CsrfOriginException()
-            }
+        onFailure { message ->
+            respond(
+                HttpStatusCode.Forbidden,
+                when {
+                    message.startsWith("missing") -> ErrorFactory.create403(Error403Cause.Kind.CsrfHeaderRequired)
+                    message.startsWith("unexpected") -> ErrorFactory.create403(Error403Cause.Kind.CsrfTokenRequired)
+                    else -> ErrorFactory.create403()
+                },
+            )
+
         }
     }
 }
