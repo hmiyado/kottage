776902c276a4f64103e22af0f2d807797858868e
miyado <10195648+hmiyado@users.noreply.github.com>
2025-09-20 14:58:29 +0900

    fix: Restore a file removed unexpectedly

---
+++ backend/src/main/kotlin/com/github/hmiyado/kottage/model/validator/InvalidParametersException.kt
@@ -0,0 +1,5 @@
+package com.github.hmiyado.kottage.model.validator
+
+data class InvalidParametersException(
+    val parameters: List<String>,
+) : IllegalArgumentException("${parameters.joinToString(",")} is invalid")
